c4f03d3fa5138f0c16d0f68b44f6413e840e79c9
송태기 <taegi@songtaegiui-MacBookPro.local>
2022-03-12 19:15:01 +0900

    finished loop.rs

--- src/flow_control/_loop/mod.rs
+++ src/flow_control/_loop/mod.rs
@@ -5,5 +5,25 @@ pub mod nesting_and_labels;
 pub fn execute_example() {
     print_current_module_path(module_path!());
 
+    let mut count = 0u32;
 
+    println!("Let's count until infinity!");
+
+    loop {
+        count += 1;
+
+        if count == 3 {
+            println!("three");
+
+            continue;
+        }
+
+        println!("{}", count);
+
+        if count == 5 {
+            println!("Ok, that's enough");
+
+            break;
+        }
+    }
 }
