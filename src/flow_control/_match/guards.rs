8db4aa2c16cb1ee7212f4f80493a294233c611ee
송태기 <taegi@songtaegiui-MacBookPro.local>
2022-04-23 17:03:45 +0900

    finished guards.rs

--- src/flow_control/_match/guards.rs
+++ src/flow_control/_match/guards.rs
@@ -13,4 +13,12 @@ pub fn execute_example() {
         (x, _) if x % 2 == 1 => println!("The first one is odd"),
         _ => println!("No correlation..."),
     }
+
+    let number: u8 = 4;
+
+    match number {
+        i if i == 0 => println!("Zero"),
+        i if i > 0 => println!("Greater than zero"),
+        _ => println!("Fell through"), 
+    }
 }
