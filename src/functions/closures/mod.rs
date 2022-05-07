8247155c4321f53c79c97e92ac0970caf342c05a
송태기 <taegi@songtaegiui-MacBookPro.local>
2022-05-08 02:10:33 +0900

    finished mod.rs(closures)

---
+++ src/functions/closures/mod.rs
@@ -0,0 +1,25 @@
+use super::print_current_module_path;
+
+pub fn execute_example() {
+    print_current_module_path(module_path!());
+
+    fn function(i: i32) -> i32 { i + 1 }
+
+    // closure는 `{}` 중괄호 바디를 쓰는 것은 선택적 사항이다.
+    let closure_annotated = |i: i32| -> i32 { i + 1 };
+    // 타입 명시가 생력되어 있으면 가장 빨리 사용하는 곳에 의해 추론된다.
+    // 이 경우엔 아래의 i 변수가 될 것이다.
+    let closure_inferred = |i| i + 1;
+
+    let i = 1;
+
+    println!("function: {}", function(i));
+    println!("closure_annotated: {}", closure_annotated(i));
+    println!("closure_inferred: {}", closure_inferred(i));
+
+    // `i32`를 반환하는 인자를 취하지 않는 클로저
+    // 반환 타입은 추론된다.
+    let one = || 1;
+    println!("closure returning one: {}", one());
+
+}
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     println!("[ ------------ Start Main of rustExample ------------ ]");
     println!("[ --------------------------------------------------- ]");
 
-    // hello_world
+    // 01. hello_world
     // rustExample::hello_world::execute_example();
     // rustExample::hello_world::comments::execute_example();
     // rustExample::hello_world::formatted_print::execute_example();
@@ -16,13 +16,13 @@ fn main() {
     // rustExample::hello_world::formatted_print::display::testcase_list::execute_example();
     // rustExample::hello_world::formatted_print::formatting::execute_example();
     
-    // primitives
+    // 02. primitives
     // rustExample::primitives::execute_example();
     // rustExample::primitives::literal_and_operators::execute_example();
     // rustExample::primitives::tuples::execute_example();
     // rustExample::primitives::arrays_and_slices::execute_example();
     
-    // custom_types
+    // 03. custom_types
     // rustExample::custom_types::structures::execute_example();
     // rustExample::custom_types::enums::execute_example();
     // rustExample::custom_types::enums::use_::execute_example();
@@ -30,27 +30,27 @@ fn main() {
     // rustExample::custom_types::enums::testcase_linked_list::execute_example();
     // rustExample::custom_types::constants::execute_example();
     
-    // variable_bindings
+    // 04. variable_bindings
     // rustExample::variable_bindings::execute_example();
     // rustExample::variable_bindings::mutability::execute_example();
     // rustExample::variable_bindings::scope_and_shadowing::execute_example();
     // rustExample::variable_bindings::declare_first::execute_example();
     
-    // casting
+    // 05. casting
     // rustExample::casting::execute_example();
     // rustExample::casting::literals::execute_example();
     // rustExample::casting::inference::execute_example();
     // rustExample::casting::alias::execute_example();
     
-    // expressions
+    // 06. expressions
     // rustExample::expressions::execute_example();
     
-    // coversion
+    // 07. coversion
     // rustExample::conversion::from_and_into::execute_example();
     // rustExample::conversion::tryfrom_and_tryinto::execute_example();
     // rustExample::conversion::to_and_from_strings::execute_example();
     
-    // flow_control
+    // 08. flow_control
     // rustExample::flow_control::if_else::execute_example();
     // rustExample::flow_control::_loop::execute_example();
     // rustExample::flow_control::_loop::nesting_and_labels::execute_example();
@@ -68,8 +68,9 @@ fn main() {
     // rustExample::flow_control::if_let::execute_example();
     // rustExample::flow_control::while_let::execute_example();
 
-    // functions
+    // 09. functions
     // rustExample::functions::execute_example();
-    rustExample::functions::methods::execute_example();
+    // rustExample::functions::methods::execute_example();
+    rustExample::functions::closures::execute_example();
 
 }
