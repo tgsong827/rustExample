fc9c5d7f05c18c9587b899a3a403fc5474bb7086
송태기 <taegi@songtaegiui-MacBookPro.local>
2022-06-01 16:13:41 +0900

    finished searching_through_iterators.rs_2

--- src/functions/closures/examples_in_std/searching_through_iterators.rs
+++ src/functions/closures/examples_in_std/searching_through_iterators.rs
@@ -23,7 +23,7 @@ pub fn execute_example() {
     
     let index_of_first_even_number = vec.iter().position(|&x| x % 2 == 0);
     assert_eq!(index_of_first_even_number, Some(5));
-
+    
     let index_of_first_negative_number = vec.into_iter().position(|x| x < 0);
     assert_eq!(index_of_first_negative_number, None);
 }
