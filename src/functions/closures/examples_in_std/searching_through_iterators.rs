8a4cbda2c26d6e89c732c2f0e65d776530603ca9
송태기 <taegi@songtaegiui-MacBookPro.local>
2022-06-01 16:06:38 +0900

    finished searching_through_iterators.rs

---
+++ src/functions/closures/examples_in_std/searching_through_iterators.rs
@@ -0,0 +1,29 @@
+use super::print_current_module_path;
+
+pub fn execute_example() {
+    print_current_module_path(module_path!());
+
+    let vec1 = vec![1, 2, 3];
+    let vec2 = vec![4, 5, 6];
+
+    let mut iter = vec1.iter();
+    let mut into_iter = vec2.into_iter();
+
+    println!("Find 2 in vec1: {:?}", iter.find(|&&x| x == 2));
+    println!("Find 2 in vec2: {:?}", into_iter.find(| &x| x == 2));
+
+    let array1 = [1, 2, 3];
+    let array2 = [4, 5, 6];
+
+    println!("Find 2 in array1: {:?}", array1.iter().find(|&&x| x == 2));
+    // println!("Find 2 in array2: {:?}", array2.into_iter().find(|&x| x == 2));
+
+    // ---------------------------------------------
+    let vec = vec![1, 9, 3, 3, 13, 2];
+    
+    let index_of_first_even_number = vec.iter().position(|&x| x % 2 == 0);
+    assert_eq!(index_of_first_even_number, Some(5));
+
+    let index_of_first_negative_number = vec.into_iter().position(|x| x < 0);
+    assert_eq!(index_of_first_negative_number, None);
+}
--- src/main.rs
+++ src/main.rs
@@ -77,6 +77,7 @@ fn main() {
     // rustExample::functions::closures::type_anonymity::execute_example();
     // rustExample::functions::closures::input_functions::execute_example();
     // rustExample::functions::closures::as_output_parameters::execute_example();
-    rustExample::functions::closures::examples_in_std::iterator_any::execute_example();
+    // rustExample::functions::closures::examples_in_std::iterator_any::execute_example();
+    rustExample::functions::closures::examples_in_std::searching_through_iterators::execute_example();
 
 }
