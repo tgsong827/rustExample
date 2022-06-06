a24787cc0438d88ccb3a0341a8209c24d1b1ffc1
송태기 <taegi@songtaegiui-MacBookPro.local>
2022-06-06 18:17:28 +0900

    finished diverging_functions.rs

--- src/functions/diverging_functions.rs
+++ src/functions/diverging_functions.rs
@@ -16,4 +16,20 @@ pub fn execute_example() {
 
     // ....
     // 아직 진행중..
+    // let x: ! = panic!("This call never returns.");
+    // println!("You will never see this line!");
+    
+    fn sum_odd_numbers(up_to: u32) -> u32 {
+        let mut acc = 0;
+        for i in 0..up_to {
+            let addition: u32 = match i%2 == 1 {
+                true => i,
+                false => continue,
+            };
+            acc += addition;
+        }
+        acc
+    }
+
+    println!("Sum of odd numbers up to 9 (excluding): {}", sum_odd_numbers(9));
 }
